5caf6c91c2d377c9a3b709abde912740ca52d18e
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2023-08-14 11:00:01 +0900

    Mint画面(仮）を作成。

---
+++ pages/mint.tsx
@@ -0,0 +1,22 @@
+import { useContract, useNFTs } from "@thirdweb-dev/react";
+import React from "react";
+import Container from "../components/Container/Container";
+import NFTGrid from "../components/NFT/NFTGrid";
+import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
+
+export default function Mint() {
+  // Load all of the NFTs from the NFT Collection
+  const { contract } = useContract(NFT_COLLECTION_ADDRESS);
+  const { data, isLoading } = useNFTs(contract);
+
+  return (
+    <Container maxWidth="lg">
+      <h1>Mint NFTs</h1>
+      <p>Lets make your NFT.</p>
+
+      
+
+
+    </Container>
+  );
+}
