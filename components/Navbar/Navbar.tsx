65720404da5e3c62c5f961a1330d8a17d5206888
Hira-Nob <dendendenso.2@gmail.com>
2023-08-15 17:27:45 +0900

    TOPデザイン変更_学習

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
         <div className={styles.navLeft}>
           <Link href="/" className={`${styles.homeLink} ${styles.navLeft}`}>
             <Image
-              src="/logo.png"
+              src="/0_iCon_256.png"
               width={48}
               height={48}
               alt="NFT marketplace sample logo"
